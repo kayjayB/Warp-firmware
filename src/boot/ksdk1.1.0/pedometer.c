c9a19ffe0ebdd4c5700a59679f95d4f2bc84d501
Kayla-Jade Butkow <kaylabutkow@gmail.com>
2020-01-13 16:11:30 +0000

    New pedometer attempt. Doesn't work

--- src/boot/ksdk1.1.0/pedometer.c
+++ src/boot/ksdk1.1.0/pedometer.c
@@ -28,21 +28,79 @@ Function: pedometer
 void pedometer()
 {
 	calibratePedometer();
-
+    int16_t arrNumbersX[4] = {0};
+    int16_t arrNumbersY[4] = {0};
+    int16_t arrNumbersZ[4] = {0};
+    uint16_t filterCounter = 0;
+    uint8_t smoothingNumber = 4;
     while (1)
     {
-        for (uint8_t i=1; i<numReadings; i++)
+        maxX = -10000;
+        minX = 10000;
+        maxY = -10000;
+        minZ = 10000;
+        maxZ = -10000;
+        minZ = 10000;
+        for (uint8_t i=0; i<numReadings; i++)
         {
-            float tempResult = resultant[1];
-            success = getSensorDataMMA8451Q(acceleration);
-            X_acc[i] = acceleration[0];
-            Y_acc[i] = acceleration[1];
-            Z_acc[i] = acceleration[2];
+            // Read sensor data
+            getSensorDataMMA8451Q(acceleration);
+            OSA_TimeDelay(delay1);
 
-            resultant[1] = sqrt(((X_acc[i] - X_avg) * (X_acc[i] - X_avg)) + ((Y_acc[i] - Y_avg) * (Y_acc[i] - Y_avg)) + ((Z_acc[i] - Z_avg) * (Z_acc[i] - Z_avg)));
-            resultant[0] = tempResult;
-            average = (resultant[1] + resultant[0]) / 2 ;
+            // Perform moving average filtering on the samples
+            X_acc[i] = movingAverage(arrNumbersX, &sumX, filterCounter, smoothingNumber, acceleration[0] - X_avg);
+            Y_acc[i] = movingAverage(arrNumbersY, &sumY, filterCounter, smoothingNumber, acceleration[1] - Y_avg);
+            Z_acc[i]  = movingAverage(arrNumbersZ, &sumZ, filterCounter, smoothingNumber, acceleration[2] - Z_avg);
+
+            if (X_acc[i] >= maxX)
+            {
+                maxX = X_acc[i];
+            }
+            if (X_acc[i] < minX)
+            {
+                minX = X_acc[i];
+            }
+            if (Y_acc[i] >= maxY)
+            {
+                maxY = Y_acc[i];
+            }
+            if (Y_acc[i] < minY)
+            {
+                minY = Y_acc[i];
+            }
+            if (Z_acc[i] >= maxZ)
+            {
+                maxZ = Z_acc[i];
+            }
+            if (Z_acc[i] < minZ)
+            {
+                minZ = Z_acc[i];
+            }
+
+            filterCounter++;
+            filterCounter = filterCounter % smoothingNumber;
+
+            switch (activeAxis) {
+                case 1:
+                    average = (X_acc[i]);  //+ X_acc[i-1])/2;
+                    break;
+                case 2:
+                    average = (Y_acc[i] );//+ Y_acc[i-1])/2;
+                    break;
+                case 3:
+                    average = (Z_acc[i] );// + Z_acc[i-1])/2;
+                    break;
+                default:
+                    average = (Z_acc[i]);// + Z_acc[i-1])/2;
+                    break;
+            }
+
             SEGGER_RTT_printf(0, "Average: %d \n", (int)average);
+
+            SEGGER_RTT_printf(0, "Threshold: %d \n", (int)threshold);
+
+            // SEGGER_RTT_printf(0, "Active axis: %d \n", (int)activeAxis);
+
             if (average > threshold && flag==false)
             {
                 steps=steps+1;
@@ -58,8 +116,27 @@ void pedometer()
                 display(steps, prevVal);
             }
 
-            OSA_TimeDelay(delay);
         }
+
+        // findMin_Max(X_acc, numReadings, &minX, &maxX);
+        // findMin_Max(Y_acc, numReadings, &minY, &maxY);
+        // findMin_Max(Z_acc, numReadings, &minY, &maxY);
+
+        if (((maxX - minX) >= (maxY - minY)) && ((maxX - minX) >= (maxZ - minZ)))
+        {
+            activeAxis = 1;
+            threshold = (maxX + minX)/2;
+        } else if (((maxY - minY) >= (maxX - minX)) && ((maxY - minY) >= (maxZ - minZ)))
+        {
+            activeAxis = 2;
+            threshold = (maxY + minY)/2;
+        } else 
+        {
+            activeAxis = 3;
+            threshold = (maxZ + minZ)/2;
+        }
+
+        OSA_TimeDelay(delay2);
     }
     
 }
@@ -73,33 +150,85 @@ Function: calibratePedometer
 */
 void calibratePedometer()
 {
-    devSSD1331init();
 
-    int sum_X = 0, sum_Y=0 , sum_Z= 0;
+    int16_t sum_X = 0, sum_Y=0 , sum_Z= 0;
+    maxX = -10000;
+    minX = 10000;
+    maxY = -10000;
+    minZ = 10000;
+    maxZ = -10000;
+    minZ = 10000;
 
     for (uint8_t i=0; i<numReadings; i++)
     {
-        success = getSensorDataMMA8451Q(acceleration);
-        OSA_TimeDelay(delay);
+        getSensorDataMMA8451Q(acceleration);
+        OSA_TimeDelay(delay1);
+        OSA_TimeDelay(delay1);
         X_acc[i] = acceleration[0];
         sum_X += X_acc[i];
         Y_acc[i] = acceleration[1];
         sum_Y += Y_acc[i];
         Z_acc[i] = acceleration[2];
         sum_Z += Z_acc[i];
+
+        if (X_acc[i] > maxX)
+        {
+            maxX = X_acc[i];
+        }
+        if (X_acc[i] < minX)
+        {
+            minX = X_acc[i];
+        }
+        if (Y_acc[i] > maxY)
+        {
+            maxY = Y_acc[i];
+        }
+        if (Y_acc[i] < minY)
+        {
+            minY = Y_acc[i];
+        }
+        if (Z_acc[i] > maxZ)
+        {
+            maxZ = Z_acc[i];
+        }
+        if (Z_acc[i] < minZ)
+        {
+            minZ = Z_acc[i];
+        }
     }
 
-    X_avg = sum_X/numReadingsF;
-    Y_avg = sum_Y/numReadingsF;
-    Z_avg = sum_Z/numReadingsF;
+    X_avg = sum_X/numReadings;
+    Y_avg = sum_Y/numReadings;
+    Z_avg = sum_Z/numReadings;
+
+    maxX = maxX - X_avg;
+    minX = minX - X_avg;
+    maxY = maxY - Y_avg;
+    minY = minY - Y_avg;
+    maxZ = maxZ - Z_avg;
+    minZ = minZ - Z_avg;
+
+    if (((maxX - minX) >= (maxY - minY)) && ((maxX - minX) >= (maxZ - minZ)))
+    {
+        activeAxis = 1;
+        threshold = (maxX + minX)/2;
+    } else if (((maxY - minY) >= (maxX - minX)) && ((maxY - minY) >= (maxZ - minZ)))
+    {
+        activeAxis = 2;
+        threshold = (maxY + minY)/2;
+    } else 
+    {
+        activeAxis = 3;
+        threshold = (maxZ + minZ)/2;
+    }
 
     SEGGER_RTT_printf(0, "X_avg: %d\t Y_avg: %d\t Z_avg: %d\n", (int)X_avg, (int)Y_avg, (int)Z_avg);
 
-    resultant[0] = sqrt(((X_acc[numReadings-1] - X_avg) * (X_acc[numReadings-1] - X_avg)) + ((Y_acc[numReadings-1] - Y_avg) * (Y_acc[numReadings-1] - Y_avg)) + ((Z_acc[numReadings-1] - Z_avg) * (Z_acc[numReadings-1] - Z_avg)));
+    devSSD1331init();
 
 }
 
-int movingAverage(int *window, long *sum, int pos, int numSamples, int nextNum)
+int movingAverage(int16_t *window, long *sum, uint16_t pos, uint8_t numSamples, int16_t nextNum)
 {
   //Subtract the oldest number from the prev sum, add the new number
   *sum = *sum - window[pos] + nextNum;
@@ -108,12 +237,3 @@ int movingAverage(int *window, long *sum, int pos, int numSamples, int nextNum)
   //return the average
   return *sum / numSamples;
 }
-
-    sum -= reads[ptr];
-    sum += heartRateValue;
-    reads[ptr] = heartRateValue;
-    ave = sum / samp_siz;
-    //Serial.println(heartRateValue);
-    //Serial.println(ave);
-    ptr++;
-    ptr %= samp_siz;
--- src/boot/ksdk1.1.0/pedometer.h
+++ src/boot/ksdk1.1.0/pedometer.h
@@ -3,22 +3,34 @@ volatile WarpI2CDeviceState			deviceMMA8451QState;
 #endif
 
 void calibratePedometer();
+int movingAverage(int16_t *window, long *sum, uint16_t pos, uint8_t numSamples, int16_t nextNum);
 void pedometer();
-int movingAverage(int *window, long *sum, int pos, int numSamples, int nextNum);
 
-int numReadings = 20;
-float numReadingsF = 20.0;
+uint8_t numReadings = 20;
 
 int16_t acceleration[3];
 int16_t X_acc[20];
 int16_t Y_acc[20];
 int16_t Z_acc[20];
-float resultant[2];
-float average=0.0;
-int delay=0500; //  50 ms delay
-bool success = true;
+
+int16_t minX;
+int16_t maxX;
+int16_t minY;
+int16_t maxY;
+int16_t minZ;
+int16_t maxZ;
+
+uint8_t activeAxis=2;
+
+int16_t threshold;
+
+long sumX=0;
+long sumY=0;
+long sumZ=0;
+int16_t average=0;
+uint16_t delay1=1; //  1 ms delay
+uint16_t delay2=50; //  50 ms delay
 bool flag = false;
-float X_avg, Y_avg, Z_avg;
+int X_avg, Y_avg, Z_avg;
 uint16_t steps =  0;
-float threshold = 100;
 uint16_t I2cPullupValue = 32768;
